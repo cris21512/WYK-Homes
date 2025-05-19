6872826133f55fc4c89e8cb9cbbed3681a31300e
Master21 <mastercrack2112.5@gmail.com>
2025-05-19 09:07:44 -0600

    Show contact

--- WYK_Homes/src/components/Contact/Contact.jsx
+++ WYK_Homes/src/components/Contact/Contact.jsx
@@ -6,19 +6,27 @@ const Contact = () => {
   return (
     <div className='Contact-style'>
         <section className='house-blur'/>
-        <div clasName='footer-container'>
-          <section className='footer-column'>
-            <img src={phone} alt="phone" />
-            <h3>WYK Homes</h3>
+        <div className='contact-container'>
+          <section>
+            <h1>hola a todos</h1>
+            <h2>¿Tienes alguna pregunta?</h2>
+            <h3>¡No dudes en contactarnos!</h3>
+            <h1>joder</h1>
+            <h2>quee??</h2>
           </section>
-          <div className='footer-column'>
-            <h1>Phone</h1>
-            <p>+1 (123) 456-7890</p>
-            <p>+1 (123) 456-7890</p>
-          </div>
-          <section className='footer-column'>
-            <h1>Email</h1>
-            <p>aqui nos contactas con el correo putoelquelee@hotmail</p>
+          <section>
+            <h1>hola a todos</h1>
+            <h2>¿Tienes alguna pregunta?</h2>
+            <h3>¡No dudes en contactarnos!</h3>
+            <h1>joder</h1>
+            <h2>quee??</h2>
+          </section>
+          <section>
+            <h1>hola a todos</h1>
+            <h2>¿Tienes alguna pregunta?</h2>
+            <h3>¡No dudes en contactarnos!</h3>
+            <h1>joder</h1>
+            <h2>quee??</h2>
           </section>
         </div>
     </div>
